d13f71f803def0d8c394f26abc29ffc33e6e4324
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-28 15:18:48 -0400

    Update Sample9Client.java

--- src/main/java/com/co/services/sample/client/Sample9Client.java
+++ src/main/java/com/co/services/sample/client/Sample9Client.java
@@ -3,7 +3,7 @@ package com.co.services.sample.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "sample9-service")
+@FeignClient(name = "sample-service9")
 public interface Sample9Client {
 
 	@GetMapping("/")
