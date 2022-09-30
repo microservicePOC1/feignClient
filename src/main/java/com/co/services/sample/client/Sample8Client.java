77630dff8b3400bdcd26f13b9658546c5c0a6ebf
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-30 15:40:26 -0400

    Update Sample8Client.java

--- src/main/java/com/co/services/sample/client/Sample8Client.java
+++ src/main/java/com/co/services/sample/client/Sample8Client.java
@@ -1,12 +1,12 @@
-// package com.co.services.sample.client;
+package com.co.services.sample.client;
 
-// import org.springframework.cloud.openfeign.FeignClient;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
-// @FeignClient(name = "sample-service8")
-// public interface Sample8Client {
+@FeignClient(name = "sample-service8")
+public interface Sample8Client {
 
-// 	@GetMapping("/")
-// 	String getService();
+	@GetMapping("/")
+	String getService();
 	
-// }
+}
