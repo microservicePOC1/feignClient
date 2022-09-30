54b14b81371f1403eb888b4f924459bc07086b12
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-30 15:42:21 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -11,9 +11,9 @@ import com.co.services.sample.client.Sample3Client;
 import com.co.services.sample.client.Sample4Client;
 import com.co.services.sample.client.Sample5Client;
 import com.co.services.sample.client.Sample6Client;
-// import com.co.services.sample.client.Sample7Client;
-// import com.co.services.sample.client.Sample8Client;
-// import com.co.services.sample.client.Sample9Client;
+import com.co.services.sample.client.Sample7Client;
+import com.co.services.sample.client.Sample8Client;
+import com.co.services.sample.client.Sample9Client;
 
 @RestController
 public class SampleController {
@@ -24,9 +24,9 @@ public class SampleController {
 	Sample4Client sample4Client;
 	Sample5Client sample5Client;
 	Sample6Client sample6Client;
-// 	Sample7Client sample7Client;
-// 	Sample8Client sample8Client;
-// 	Sample9Client sample9Client;
+	Sample7Client sample7Client;
+	Sample8Client sample8Client;
+	Sample9Client sample9Client;
 
 	public SampleController(
 		Sample2Client sample2Client,
@@ -34,18 +34,18 @@ public class SampleController {
 		Sample4Client sample4Client,
 		Sample5Client sample5Client,
 		Sample6Client sample6Client
-// 		Sample7Client sample7Client,
-// 		Sample8Client sample8Client,
-// 		Sample9Client sample9Client
+		Sample7Client sample7Client,
+		Sample8Client sample8Client,
+		Sample9Client sample9Client
 	) {
 		this.sample2Client = sample2Client;
 		this.sample3Client = sample3Client;
 		this.sample4Client = sample4Client;
 		this.sample5Client = sample5Client;
 		this.sample6Client = sample6Client;
-// 		this.sample7Client = sample7Client;
-// 		this.sample8Client = sample8Client;
-// 		this.sample9Client = sample9Client;
+		this.sample7Client = sample7Client;
+		this.sample8Client = sample8Client;
+		this.sample9Client = sample9Client;
 	}
 	
 	@GetMapping("/")
@@ -56,10 +56,10 @@ public class SampleController {
 				() -> sample3Client.getService(),
 				() -> sample4Client.getService(),
 				() -> sample5Client.getService(),
-				() -> sample6Client.getService()
-// 				() -> sample7Client.getService(),
-// 				() -> sample8Client.getService(),
-// 				() -> sample9Client.getService()
+				() -> sample6Client.getService(),
+				() -> sample7Client.getService(),
+				() -> sample8Client.getService(),
+				() -> sample9Client.getService()
 			);
 
 			String result = "";
@@ -74,10 +74,10 @@ public class SampleController {
 			sample3Client.getService() + "<br/>" +
 			sample4Client.getService() + "<br/>" +
 			sample5Client.getService() + "<br/>" +
-			sample6Client.getService() + "<br/>"
-// 			sample7Client.getService() + "<br/>" +
-// 			sample8Client.getService() + "<br/>" +
-// 			sample9Client.getService()
+			sample6Client.getService() + "<br/>" +
+			sample7Client.getService() + "<br/>" +
+			sample8Client.getService() + "<br/>" +
+			sample9Client.getService()
 		);
 }
 	
